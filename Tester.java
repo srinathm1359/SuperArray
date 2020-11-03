6a2a4aae786fc5499585f6fc93b414430df2785a
Srinath Mahankali <smahankali10@stuy.edu>
2020-11-03 05:44:04 -0500

    Tested add()

--- Tester.java
+++ Tester.java
@@ -3,5 +3,8 @@ public class Tester {
   public static void main(String[] args) {
     SuperArray first_dynamic = new SuperArray();
     System.out.println(first_dynamic.size());
+    first_dynamic.add("Hello");
+    first_dynamic.add("Hello again!");
+    System.out.println(first_dynamic.size());
   }
 }
