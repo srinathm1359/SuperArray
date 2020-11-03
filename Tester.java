69406796a5bff5b8c5bc8d738fdc76e2e388be6a
Srinath Mahankali <smahankali10@stuy.edu>
2020-11-03 05:47:03 -0500

    Tested get()

--- Tester.java
+++ Tester.java
@@ -2,9 +2,15 @@
 public class Tester {
   public static void main(String[] args) {
     SuperArray first_dynamic = new SuperArray();
+    //Testing size()
     System.out.println(first_dynamic.size());
+    //Testing add()
     first_dynamic.add("Hello");
     first_dynamic.add("Hello again!");
     System.out.println(first_dynamic.size());
+    //Testing get()
+    System.out.println(first_dynamic.get(0));
+    System.out.println(first_dynamic.get(1));
+    System.out.println(first_dynamic.get(2));
   }
 }
