21e25054e44cbd61907b0ecfd79a22d298e99bf4
Srinath Mahankali <smahankali10@stuy.edu>
2020-11-05 05:48:40 -0500

    Added indexOf()

--- SuperArray.java
+++ SuperArray.java
@@ -112,4 +112,13 @@ public class SuperArray {
     size--;
     return null;
   }
+
+  public int indexOf(String s) {
+    for (int i = 0; i < size; i++) {
+      if (data[i].equals(s)) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
